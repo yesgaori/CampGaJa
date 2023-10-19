5ee482691c982571f7907f3a38d081df17e361ce
DESKTOP-O99DVCC\user <khy20100065@gmail.com>
2023-10-19 16:19:46 +0900

    [강하영]  - campgaja project 첫 깃파일 생성

---
+++ src/main/java/com/campgaja/CampGaJaApplication.java
@@ -0,0 +1,13 @@
+package com.campgaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampGaJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampGaJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campgaja/CampGaJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campgaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampGaJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
